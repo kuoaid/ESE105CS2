06c93eba1f9261d57ef635ca9da210e9f6287cab
Kuo Wang <kuowang@MacBook-Pro.lan>
2022-10-28 22:34:32 -0500

    fix bug

--- Part_1.m
+++ Part_1.m
@@ -1,6 +1,8 @@
-for index = 0
+result = [];
+x0 = [1; 0; 0; 0];
+for index = 1:100
 
-x0 = [1; 0; 0; 0];  % Initial population is a hundred percent susceptible
+  % Initial population is a hundred percent susceptible
                     % to the new disease before the start of the epidemic.
                     % The four entries correspond to the portion of 
                     % susceptible, infected, recovered and deceased
@@ -8,7 +10,7 @@ x0 = [1; 0; 0; 0];  % Initial population is a hundred percent susceptible
 
 A = [0.2, 0.2,  0, 0;    % A is the update matrix of how the disease evolve
      0.8, 0,    0, 0;    % within the population. Note that the 4th column of
-     0,   0.01, 0, 0;    % the matrix is all 0 except for the last entry since
+     0,   0.01, 1, 0;    % the matrix is all 0 except for the last entry since
      0,   0.79, 0, 1];   % deceased people must remain dead. Also, each column
                          % must add up to 1 since we are assuming that the
                          % population is a closed population.
@@ -17,4 +19,6 @@ xt = A * x0;        % The updated version of the infectious disease after
                     % one unit time in the population.
 
 x0 = xt;
+
+result = [result xt];
 end

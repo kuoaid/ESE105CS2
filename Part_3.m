7cd456545bff3ebd9bda19cb9aeb5390633aee37
unknown <lin.xinzhu@wustl.edu>
2022-11-06 15:45:28 -0600

    n

--- Part_3.m
+++ Part_3.m
@@ -17,16 +17,25 @@ x0 = [0.0812, 0.011, 0.59, 0.01, 0.0001, 1, 0, 0, 0, 0];
 x = fmincon(sirafun,x0,A,b,Af,bf,lb,ub);
 
 Y_fit = siroutput_full_withVacc(x,t);
-Y_fit_D = ones([t,1])-Y_fit(1:t,1)-Y_fit(1:t,5);
-Y_fit_C = Y_fit(1:t,4);
+Y_fit_C = ones([t,1])-Y_fit(1:t,1)-Y_fit(1:t,5);
+Y_fit_D = Y_fit(1:t,4);
 
 figure;
 hold on;
 plot(coviddata);
-plot(Y_fit_D);
 plot(Y_fit_C);
+plot(Y_fit_D);
 hold off;
 title("Mock COVID data");
 xlabel("Days");
 ylabel("Fraction of total population");
-legend('Deaths','Cases','Y fit deaths','Y fit cases');
+legend('newInfections','cumulativeDeaths','Y fit cases','Y fit deaths');
+%%
+vaxpop = Y_fit(:,5);
+vaxbreak = ones([t,1])-Y_fit(1:t,1)-Y_fit(1:t,5);
+figure;
+hold on;
+plot(vaxpop);
+plot(vaxbreak);
+hold off;
+legend('vaxpop','vaxbreak');
